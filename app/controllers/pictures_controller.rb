4be3a1ae02db703fc6768931769ce4f02fc3298c
Ken Mazaika <kmazaika@gmail.com>
2012-04-01 20:56:40 -0400

    Start allow creating of pictures.

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,10 @@
+class PicturesController < ApplicationController
+  def index
+    @pictures = Picture.all
+  end
+
+
+  def new
+    @picture = Picture.new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Pictures::Application.routes.draw do
+  resources :pictures
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
